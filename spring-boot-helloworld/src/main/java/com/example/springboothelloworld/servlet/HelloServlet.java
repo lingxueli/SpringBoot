56ee04fb576b064bde4244a0a595f77ffbe7540a
lingxueli <ll2871@columbia.edu>
2019-08-07 22:39:37 -0600

    Add filter for servlet http request handling method

--- spring-boot-helloworld/src/main/java/com/example/springboothelloworld/servlet/HelloServlet.java
+++ spring-boot-helloworld/src/main/java/com/example/springboothelloworld/servlet/HelloServlet.java
@@ -1,9 +1,12 @@
 package com.example.springboothelloworld.servlet;
 
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name="helloServlet", urlPatterns ="/helloServlet")
 public class HelloServlet extends HttpServlet {
